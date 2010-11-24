33d8e9ce8be2901dab5998192559b0e1c3408807
Igor Davydenko <playpauseandstop@gmail.com>
2010-11-24 14:32:24 +0200

    Make ``path`` context processor return request absolute URI too.

--- kikola/core/context_processors.py
+++ kikola/core/context_processors.py
@@ -1,15 +1,13 @@
 def path(request):
     """
-    kikola.core.context_processors.path
-    ===================================
-
-    Adds current path and full path variables to templates.
+    Adds current absolute URI, path and full path variables to templates.
 
     To enable, adds ``kikola.core.context_processors.path`` to your project's
     ``settings`` ``TEMPLATE_CONTEXT_PROCESSORS`` var.
 
     **Note:** Django has ``django.core.context_processors.request`` context
-    processor that adding current ``HttpRequest`` object to templates.
+    processor that adding whole ``HttpRequest`` object to templates.
     """
-    return {'REQUEST_FULL_PATH': request.get_full_path(),
+    return {'REQUEST_ABSOLUTE_URI': request.build_absolute_uri(),
+            'REQUEST_FULL_PATH': request.get_full_path(),
             'REQUEST_PATH': request.path}
